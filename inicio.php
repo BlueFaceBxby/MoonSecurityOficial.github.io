97c2060e832d7121bcc7342e8ab7b063f7782b03
BlueFaceBxby <131032837+BlueFaceBxby@users.noreply.github.com>
2025-02-03 21:50:07 -0500

    Create inicio.php

---
+++ inicio.php
@@ -0,0 +1,49 @@
+<?php
+
+session_start();
+  if(!isset($_SESSION['usuario'])){
+    echo '
+     <script>
+       alert("Porfavor debes iniciar sesion.");
+       window.location = "../index.php";
+    </script>
+    ';
+    header("location: index.php");
+    session_destroy();
+    die();
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moon Security</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="banner">
+        <div class="navbar">
+            <a href="inicio.php"><img src="logo_moon.jpg" class="logo"></a>
+            <ul>
+                <li><a href="nosotros.html">NOSOTROS</a></li>
+                <li><a href="reportes.html">REPORTES</a></li>
+                <li><a href="consejos.html">CONSEJOS</a></li>
+                <li><a href="chatbot.html">MOON BOT</a></li>
+                <li><a href="contactanos.html">CONTACTANOS</a></li>
+                <li><a href="php/cerrar_sesion.php">CERRAR SESION</a></li>
+            </ul>
+
+        </div>
+        <div class="content">
+            <h1>MOON SECURITY</h1>
+            <p>Protegiendo tu voz, construyendo un entorno seguro.</p>
+            <div>
+                <a href="nosotros.html"><button type="button"><span></span>NOSOTROS</button></a>
+                <a href="contactanos.html"><button type="button"><span></span>CONTACTANOS</button></a>
+             </div>    
+        </div>
+    </div>
+</body>
+</html>
